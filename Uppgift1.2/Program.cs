6fb27c62a191dc06e775b9eee75b2e99d72a1cfd
johe07003 <johe07003@E-PW05C37X>
2024-09-12 12:37:08 +0200

    Add project files.

---
+++ Uppgift1.2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace Hej
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.White;
+            //Du är nästan klar med kapitel 1.//
+            Console.WriteLine("ERRORRRRRR!!!!!!!");
+            Console.WriteLine("Din dators bios har exploderat");
+            Console.WriteLine("Turn this PC of right now!!!!");
+            //Du har lärt dig skriva ut tecken som &quot; och \.//
+        }
+    }
+}
